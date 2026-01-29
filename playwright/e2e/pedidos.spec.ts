7f785a97aa30a2eccbb929898d2572441bef4268
Jessica Sobral <jessica.feerreira@gmail.com>
2026-01-29 17:18:05 -0300

    Iniciando a automação dos testes

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar pedido', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  //Checkpoint 1
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  //Checkpoint 
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  await page.getByTestId('search-order-id').fill('VLO-4I3TZQ');
+  
+  await page.getByTestId('search-order-button').click();
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-4I3TZQ');
+  
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+ 
+});
