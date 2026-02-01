cdb3c6536b458c8fc366f40f04fa4c70953f525d
Jessica Sobral <jessica.feerreira@gmail.com>
2026-02-01 19:41:29 -0300

    Arrange, Act, Assert (Preparar, Agir, Verificar)

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,21 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert (Preparar, Agir, Verificar)
+
 test('deve consultar pedido', async ({ page }) => {
+    //Arrange
   await page.goto('http://localhost:5173/');
-
-  //Checkpoint 1
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint 
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-4I3TZQ');
-  
   await page.getByTestId('search-order-button').click();
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-4I3TZQ');
-  
+
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
  
